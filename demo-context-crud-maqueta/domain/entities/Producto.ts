7ec2d40c327906629826210ce0cbe0cc26dffbc6
Fabricio <figueroafabricio14@gmail.com>
2026-07-07 19:23:32 -0500

    Delete demo-context-crud-maqueta directory

--- demo-context-crud-maqueta/domain/entities/Producto.ts
+++
@@ -1,9 +0,0 @@
-// PASO 2 - Definir la entidad del dominio.
-
-export interface Producto {
-  id: string;
-  nombre: string;
-  precio: number;
-}
-
-export type ProductoFormData = Omit<Producto, 'id'>;
